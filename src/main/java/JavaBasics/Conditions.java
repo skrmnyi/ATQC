eeafbc0f73f6d1b9557c03c411b5b1cf282a346a
Andriy <carhatt.xm@gmail.com>
2023-06-21 14:41:11 +0300

    gitignore changed

---
+++ src/main/java/JavaBasics/Conditions.java
@@ -0,0 +1,16 @@
+package JavaBasics;
+
+public class Conditions {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        if (a > b) {
+            System.out.println(a + " is bigger than " + b);
+        }
+        else {
+            System.out.println(b + " is bigger than " + a);
+        }
+    }
+}
+

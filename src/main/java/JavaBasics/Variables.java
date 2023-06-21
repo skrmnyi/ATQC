9466dadd34b10d5a9b15748f0d5d9cf2a067ec1a
Andriy <carhatt.xm@gmail.com>
2023-06-21 14:36:04 +0300

    variables progress

---
+++ src/main/java/JavaBasics/Variables.java
@@ -0,0 +1,37 @@
+package JavaBasics;
+
+public class Variables {
+    public static void main(String[] args) {
+
+        long longVariable = 1111l;
+        int intVariable = 1;
+        short shortVariable = 2;
+        byte byteVariable = 64;
+        double doubleVariable = 4.4;
+        float floatVariable = 4.4f;
+        char charVariabe = 'A';
+
+        boolean logic = true;
+
+        String message = "This message created for testing purpose";
+
+        int firstNumbers[] = {1, 2, 3, 4};
+
+        int lastNumbers[] = {100, 99, 98, 97};
+
+        String arrayWords[] = {"Hello", "World"};
+
+        System.out.println(longVariable);
+        System.out.println(arrayWords.toString());
+
+    }
+
+
+
+}
+
+
+
+
+
+

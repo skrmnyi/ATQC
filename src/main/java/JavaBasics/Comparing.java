a42ee7460407c7575a58869787c75ceb201f25d9
Andriy <carhatt.xm@gmail.com>
2023-06-22 15:22:29 +0300

    save progress

--- src/main/java/JavaBasics/Comparing.java
+++ src/main/java/JavaBasics/Comparing.java
@@ -21,6 +21,32 @@ class Comparing {
         boolean q = (!a || b) && (!a || !b);
         System.out.println(x);
         System.out.println(q);
+        System.out.println("_________");
+
+        int ab = 2;
+        int ba = 5;
+
+        double ca = 5.5;
+
+        if (ab > 1) {
+            System.out.println("simple > operator");
+        }
+        if (ba < 6) {
+            System.out.println("simple < operator");
+        }
+        if (ba <= 5) {
+            System.out.println("<= means less or equals");
+        }
+        if (ab >= 1) {
+            System.out.println(">= means bigger or equals");
+        }
+        if (ab == 1) {
+            System.out.println("== means equal");
+        }
+        if (ab !=5) {
+            System.out.println("!= means not equal");
+        }
+
     }
 }
 
